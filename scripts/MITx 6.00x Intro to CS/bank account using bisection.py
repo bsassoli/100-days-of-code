d0c312efd1e031cfef928a1497c2e0f96d2fb40f
Bernardino Sassoli de' Bianchi <61242474+bsassoli@users.noreply.github.com>
2020-06-21 15:05:05 +0200

    Update bank account using bisection.py
    Fixed error in while loop.

--- scripts/MITx 6.00x Intro to CS/bank account using bisection.py
+++ scripts/MITx 6.00x Intro to CS/bank account using bisection.py
@@ -1,30 +1,25 @@
-def get_balance():
-    annualInterestRate = 0.18
-    monthly_ir = annualInterestRate / 12
-    balance = 999999
+
+annualInterestRate = 0.2
+monthly_ir = annualInterestRate / 12
+balance = 320000
+updated = balance
+epsilon = 0.01
+
+lower = balance / 12
+upper = (balance * (1 + monthly_ir) ** 12) / 12.0
+min_fixed = (lower + upper)/2
+while abs(updated) > epsilon:
     updated = balance
-    epsilon = 0.01
-
-    lower = balance / 12
-    upper = (balance * (1 + monthly_ir) ** 12) / 12.0
-    min_fixed = (lower + upper)/2
-
-    while abs(updated) >= epsilon:
-        print(abs(updated) >= epsilon)
-        updated = balance
-        for month in range(1, 13):
-            monthly_outstanding = updated - min_fixed
-            updated = monthly_outstanding + (monthly_ir * monthly_outstanding)
-        if abs(updated) <= epsilon:
-            return min_fixed
-        elif updated >= epsilon:
-            upper = min_fixed
-        else:
+    for month in range(1, 13):
+        monthly_outstanding = updated - min_fixed
+        updated = monthly_outstanding + (monthly_ir * monthly_outstanding)
+    if abs(updated) < epsilon:
+        print(min_fixed)
+    else:
+        if updated > epsilon:
             lower = min_fixed
-        min_fixed = (lower + upper)/2
-        print(abs(updated) >= epsilon)
-        print(round(min_fixed, 2))
-        return round(min_fixed, 2)
-
-
-get_balance()
+        else:
+            upper = min_fixed
+    min_fixed = (lower + upper)/2
+print(abs(updated) >= epsilon)
+print(round(min_fixed, 2))
