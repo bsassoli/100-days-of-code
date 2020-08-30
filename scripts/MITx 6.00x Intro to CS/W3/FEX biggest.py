89a92d5911a93826586ef94ac52f8b20c180c8ff
Bernardino Sassoli de' Bianchi <61242474+bsassoli@users.noreply.github.com>
2020-08-30 19:48:58 +0200

    Moved MIT
    Moved MIT assignments to new repo

--- scripts/MITx 6.00x Intro to CS/W3/FEX biggest.py
+++
@@ -1,8 +0,0 @@
-def biggest(aDict):
-    '''
-    aDict: A dictionary, where all the values are lists.
-
-    returns: The key with the largest number of values associated with it
-    '''
-    lendic = {k: len(v) for (k,v) in aDict.items()}
-    return max(lendic, key=lendic.get)
